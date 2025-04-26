4fc133ef43a25b1724c1854a375530a0b35b9f44
Dalvinderpal Soora <dalvinderpaldevelopment@gmail.com>
2025-04-26 17:42:10 +0100

    Add vite config to compile scss and js files, improve footer marquee so animations are seemless

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '',
+  build: {
+    outDir: '',
+    emptyOutDir: false,
+    assetsDir: '',
+    rollupOptions: {
+      input: {
+        js: './assets/src/js/main.js',
+        scss: './assets/src/scss/main.scss'
+      },
+      output: {
+        entryFileNames: 'js/main.js',
+        chunkFileNames: 'js/main.js',
+        assetFileNames: 'css/main.css'
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+      }
+    }
+  }
+});
